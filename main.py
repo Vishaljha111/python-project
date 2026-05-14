6ca427b3334f9ac31a4f18ceb46333c83548608c
Vishal <vishaljha8434@gmail.com>
2026-05-14 10:03:08 +0530

    your message

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-name = "Vishal jha"
-first_name = name[0]
-print(first_name)
+age = int(input("How old are you?:"))
+if age >= 18:
+    print("You are an adult")
+if age == 100:
+    print("You are a century old!")
+elif age<0:
+    print("You haven't been born yet!")
+else:
+    print("You are a child!") 
