f2fd3c393e27b0c62eff37f9b5303018df3d3dd3
Vishal <vishaljha8434@gmail.com>
2026-05-15 10:17:09 +0530

    your message

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-age = int(input("How old are you?:"))
-if age >= 18:
-    print("You are an adult")
-if age == 100:
-    print("You are a century old!")
-elif age<0:
-    print("You haven't been born yet!")
-else:
-    print("You are a child!") 
+food = ["pizza","hamburger","hotdog","spaghetti"]
+print(food[1])
