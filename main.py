d1006ef828cfc9e3656d3791f8703c1bc05f6200
Vishal <vishaljha8434@gmail.com>
2026-05-20 13:54:19 +0530

    what you did

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def display_name():
-    name = "jha"  # local scope (available only inside this function)
-    print(name)
+path = "c:\\Users\\User\\Documents\\file.txt"
 
-display_name()
+if os.path.exists(path):
+    print("That location exists!")
+else:
+    print("That location does not exist!")
