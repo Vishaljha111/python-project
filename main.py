e2b8f1fe377cec10be5ba5326bacdff64dc90ad6
vishaljha111 <vishaljha8434@gmail.com>
2026-05-21 14:20:03 +0530

    what you did

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-path = "c:\\Users\\User\\Documents\\file.txt"
-
-if os.path.exists(path):
-    print("That location exists!")
-else:
-    print("That location does not exist!")
+import messages
+messages.hello()
+messages.greet("Vishal")
+messages.bye()
---
+++ messages.py
@@ -0,0 +1,8 @@
+def hello():
+    print("Hello, World!")
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+def bye():
+    print("Goodbye!")
