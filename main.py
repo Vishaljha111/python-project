8c02f46c711f8cb65614080b8443498b640521d8
vishaljha111 <vishaljha8434@gmail.com>
2026-06-04 10:22:19 +0530

    what you did today

--- main.py
+++
@@ -1,7 +0,0 @@
-﻿def add(*args):
-    total = 0
-    for arg in args:
-        total += arg
-    return total
-
-print(add(1, 2, 3, 4))
--- practice/main.py
+++
@@ -1,4 +0,0 @@
-doubles = []
-for x in range(1, 11):
-    doubles.append(x * 2)
-print(doubles)
